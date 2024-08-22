03a252b098edec85b4ed8933b7f9141c12b072b8
cyruu <cyruz.mhr09@gmail.com>
2024-08-23 00:03:25 +0545

    ok

--- src/algo/bubbleSort.js
+++ src/algo/bubbleSort.js
@@ -1,20 +1,20 @@
-export const bubbleSort = async (arr, setArr) => {
+export const bubbleSort = async (arr, setArr, speed) => {
   let sortedArr = [...arr];
-  let defaultColor = "red";
+  let defaultColor = "#ff8343";
   var j = 0;
   for (let i = 0; i < sortedArr.length - 1; i++) {
     for (j = 0; j < sortedArr.length - i - 1; j++) {
       const selected = document.querySelectorAll(".bar")[j];
       const comapring = document.querySelectorAll(".bar")[j + 1];
-      selected.style.backgroundColor = "blue";
-      comapring.style.backgroundColor = "blue";
+      selected.style.backgroundColor = "#4158A6";
+      comapring.style.backgroundColor = "#4158A6";
       if (sortedArr[j] > sortedArr[j + 1]) {
         let temp = sortedArr[j];
         sortedArr[j] = sortedArr[j + 1];
         sortedArr[j + 1] = temp;
 
         setArr([...sortedArr]);
-        await myPromise();
+        await myPromise(speed);
       }
       selected.style.backgroundColor = defaultColor;
       comapring.style.backgroundColor = defaultColor;
@@ -22,10 +22,10 @@ export const bubbleSort = async (arr, setArr) => {
   }
 };
 
-const myPromise = () => {
+const myPromise = (speed) => {
   return new Promise((res, rej) => {
     setTimeout(() => {
       res("ok");
-    }, 1);
+    }, speed);
   });
 };
--- src/sorting/SortingVisualiser.jsx
+++ src/sorting/SortingVisualiser.jsx
@@ -3,31 +3,66 @@ import { bubbleSort } from "../algo/bubbleSort";
 import "./sorting.css";
 const SortingVisualiser = () => {
   const [arr, setArr] = useState([]);
+  const [freq, setFreq] = useState(120);
+  const [speed, setSpeed] = useState(1);
+
   const randomValue = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
 
   const resetArray = () => {
     let tempArr = [];
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < freq; i++) {
       tempArr.push(randomValue(5, 500));
     }
     setArr(tempArr);
   };
   useEffect(() => {
     resetArray();
-  }, []);
+  }, [freq]);
   return (
     <div className="container">
+      <div style={{ marginBottom: "1rem" }}>
+        <button onClick={resetArray}>New Array</button>
+        <button onClick={() => bubbleSort(arr, setArr, speed)}>
+          Bubble Sort
+        </button>
+      </div>
+      <div className="options">
+        <div className="freq" style={{ marginRight: "1rem" }}>
+          <label htmlFor="frequency">Frequency</label>
+          <input
+            type="range"
+            id="frequency"
+            min="120"
+            max="220"
+            value={freq}
+            onChange={({ target }) => setFreq(target.value)}
+          />
+        </div>
+        <div className="speed">
+          <label htmlFor="speed">Speed</label>
+          <input
+            type="range"
+            id="speed"
+            min="1"
+            max="15"
+            value={speed}
+            onChange={({ target }) => setSpeed(target.value)}
+          />
+        </div>
+      </div>
       <div className="bars">
         {arr.map((el, i) => {
           return (
-            <div key={i} className="bar" style={{ height: `${el}px` }}></div>
+            <div
+              key={i}
+              className="bar"
+              style={{ height: `${el}px`, width: `4px` }}
+            ></div>
           );
         })}
       </div>
-      <button onClick={resetArray}>New Array</button>
-      <button onClick={() => bubbleSort(arr, setArr)}>Bubble Sort</button>
     </div>
   );
 };
